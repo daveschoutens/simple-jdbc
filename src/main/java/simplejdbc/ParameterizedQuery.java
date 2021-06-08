524932eb8d4f549fba61667ec17c5187c8766c20
Dave <dschoutens@gmail.com>
2021-06-07 21:48:03 -0400

    ParameterizedQuery

---
+++ src/main/java/simplejdbc/ParameterizedQuery.java
@@ -0,0 +1,67 @@
+package simplejdbc;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ParameterizedQuery {
+
+  private static final Pattern PARAM_REGEX = Pattern.compile("(?!\\B'[^']*):(\\w+)(?![^']*'\\B)");
+
+  @SuppressWarnings("rawtypes")
+  static ParameterizedQuery from(String query, Map<String, Object> bindings) {
+    Objects.requireNonNull(query, "query is required, but was null");
+    Objects.requireNonNull(bindings, "bindings required, but was null");
+
+    StringBuffer sb = new StringBuffer();
+    List<Object> parameters = new ArrayList<>();
+    Matcher m = PARAM_REGEX.matcher(query);
+    while (m.find()) {
+      String param = m.group(1);
+      if (!bindings.containsKey(param)) {
+        throw new MissingParameterBindingException(param);
+      }
+      Object value = bindings.get(param);
+      if (value instanceof Collection) {
+        StringJoiner multiParam = new StringJoiner(",");
+        for (Object subValue : (Collection) value) {
+          multiParam.add("?");
+          parameters.add(subValue);
+        }
+        m.appendReplacement(sb, multiParam.toString());
+      } else {
+        parameters.add(value);
+        m.appendReplacement(sb, "?");
+      }
+    }
+    m.appendTail(sb);
+    return new ParameterizedQuery(sb.toString(), parameters);
+  }
+
+  private final String query;
+  private final List<Object> parameters;
+
+  ParameterizedQuery(String query, List<Object> parameters) {
+    this.query = query;
+    this.parameters = parameters;
+  }
+
+  public String getSql() {
+    return query;
+  }
+
+  public List<Object> getParameters() {
+    return parameters;
+  }
+
+  public static class MissingParameterBindingException extends RuntimeException {
+    MissingParameterBindingException(String param) {
+      super(String.format("no binding provided for parameter :%s", param));
+    }
+  }
+}
---
+++ src/test/java/simplejdbc/ParameterizedQueryTest.java
@@ -0,0 +1,96 @@
+package simplejdbc;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import simplejdbc.ParameterizedQuery.MissingParameterBindingException;
+
+class ParameterizedQueryTest {
+
+  @Test
+  void withNullQuery_throws() {
+    assertThrows(
+        NullPointerException.class, () -> ParameterizedQuery.from(null, ImmutableMap.of()));
+  }
+
+  @Test
+  void withNullBindings_throws() {
+    assertThrows(NullPointerException.class, () -> ParameterizedQuery.from("", null));
+  }
+
+  @Test
+  void withNoParameters_returnsUnmodifiedQuery() {
+    String query = "select * from foo";
+
+    ParameterizedQuery result = ParameterizedQuery.from(query, ImmutableMap.of());
+
+    assertThat(result.getSql()).isEqualTo(query);
+  }
+
+  @Test
+  void withParameter_andMissingBinding_throwsSpecifyingMissingParam() {
+    RuntimeException ex =
+        assertThrows(
+            MissingParameterBindingException.class,
+            () -> ParameterizedQuery.from(":id", ImmutableMap.of()));
+    assertThat(ex).hasMessageThat().isEqualTo("no binding provided for parameter :id");
+  }
+
+  @Test
+  void withParameter_andMatchingBinding_returnsPreparableQuery() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from("select * from foo where id = :id", ImmutableMap.of("id", 123));
+
+    assertThat(result.getSql()).isEqualTo("select * from foo where id = ?");
+  }
+
+  @Test
+  void withParameter_andMatchingBinding_returnsParameterList() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from("select * from foo where id = :id", ImmutableMap.of("id", 123));
+
+    assertThat(result.getParameters()).containsExactly(123);
+  }
+
+  @Test
+  void withMultipleParameters_andMatchingBindings_returnsPreparableQueryAndParameterList() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from(
+            ":foo :bar :baz", ImmutableMap.of("foo", "FOO", "bar", "BAR", "baz", "BAZ"));
+
+    assertThat(result.getSql()).isEqualTo("? ? ?");
+    assertThat(result.getParameters()).containsExactly("FOO", "BAR", "BAZ").inOrder();
+  }
+
+  @Test
+  void withDuplicateParameters_returnsDuplicateParametersInList() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from(":foo :bar :foo", ImmutableMap.of("foo", "FOO", "bar", "BAR"));
+
+    assertThat(result.getSql()).isEqualTo("? ? ?");
+    assertThat(result.getParameters()).containsExactly("FOO", "BAR", "FOO").inOrder();
+  }
+
+  @Test
+  void withParameterBoundToCollection_returnsBindSitePerValue() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from(
+            "select * from foo where id in (:foo)", ImmutableMap.of("foo", Arrays.asList(1, 2, 3)));
+
+    assertThat(result.getSql()).isEqualTo("select * from foo where id in (?,?,?)");
+    assertThat(result.getParameters()).containsExactly(1, 2, 3).inOrder();
+  }
+
+  @Test
+  void withStringThatLooksLikeParameter_doesNotGetConfused() {
+    ParameterizedQuery result =
+        ParameterizedQuery.from(
+            "':insideString' :outsideString", ImmutableMap.of("outsideString", 123));
+
+    assertThat(result.getSql()).isEqualTo("':insideString' ?");
+    assertThat(result.getParameters()).containsExactly(123);
+  }
+}
