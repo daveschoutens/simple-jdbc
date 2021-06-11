b6e00b0ea91d1084ab7ea1606f5614758768ddc3
Dave <dschoutens@gmail.com>
2021-06-11 01:47:35 -0400

    Make SimpleJdbc abstract and provide two implementations: one for DataSource and one for Connection

--- src/main/java/simplejdbc/SimpleJdbc.java
+++ src/main/java/simplejdbc/SimpleJdbc.java
@@ -6,32 +6,112 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.sql.DataSource;
 
-public class SimpleJdbc {
+public abstract class SimpleJdbc {
 
   private final ParameterSetters parameterSetters;
   private final ColumnExtractors columnExtractors;
 
-  public SimpleJdbc(ParameterSetters parameterSetters, ColumnExtractors columnExtractors) {
+  private SimpleJdbc(ParameterSetters parameterSetters, ColumnExtractors columnExtractors) {
     this.parameterSetters = parameterSetters;
     this.columnExtractors = columnExtractors;
   }
 
-  <T> T select(
-      DataSource dataSource,
-      String sql,
-      Map<String, Object> bindings,
-      QueryResultExtractor<T> extractor) {
-    try (Connection conn = dataSource.getConnection()) {
-      return select(conn, sql, bindings, extractor);
-    } catch (SQLException ex) {
-      throw new SimpleJdbcException(ex);
+  public abstract <T> T select(
+      String sql, Map<String, Object> bindings, QueryResultExtractor<T> extractor);
+
+  public abstract int update(String sql, Map<String, Object> bindings);
+
+  public abstract int[] batchedUpdate(String sql, List<Map<String, Object>> batchedBindings);
+
+  public static SimpleJdbc using(DataSource dataSource) {
+    return new DataSourceSimpleJdbc(
+        dataSource, ParameterSetters.defaults(), ColumnExtractors.defaults());
+  }
+
+  public static SimpleJdbc using(Connection connection) {
+    return new SingleConnectionSimpleJdbc(
+        connection, ParameterSetters.defaults(), ColumnExtractors.defaults());
+  }
+
+  public static SimpleJdbc using(
+      DataSource dataSource, ParameterSetters parameterSetters, ColumnExtractors columnExtractors) {
+    return new DataSourceSimpleJdbc(dataSource, parameterSetters, columnExtractors);
+  }
+
+  public static SimpleJdbc using(
+      Connection connection, ParameterSetters parameterSetters, ColumnExtractors columnExtractors) {
+    return new SingleConnectionSimpleJdbc(connection, parameterSetters, columnExtractors);
+  }
+
+  private static class DataSourceSimpleJdbc extends SimpleJdbc {
+    private final DataSource dataSource;
+
+    private DataSourceSimpleJdbc(
+        DataSource dataSource,
+        ParameterSetters parameterSetters,
+        ColumnExtractors columnExtractors) {
+      super(parameterSetters, columnExtractors);
+      this.dataSource = dataSource;
+    }
+
+    @Override
+    public <T> T select(
+        String sql, Map<String, Object> bindings, QueryResultExtractor<T> extractor) {
+      return withNewConnection(conn -> select(conn, sql, bindings, extractor));
+    }
+
+    @Override
+    public int update(String sql, Map<String, Object> bindings) {
+      return withNewConnection(conn -> update(conn, sql, bindings));
+    }
+
+    @Override
+    public int[] batchedUpdate(String sql, List<Map<String, Object>> batchedBindings) {
+      return withNewConnection(conn -> batchedUpdate(conn, sql, batchedBindings));
+    }
+
+    private <T> T withNewConnection(Function<Connection, T> fn) {
+      try (Connection conn = dataSource.getConnection()) {
+        return fn.apply(conn);
+      } catch (SQLException ex) {
+        throw new SimpleJdbcException(ex);
+      }
     }
   }
 
-  <T> T select(
+  private static class SingleConnectionSimpleJdbc extends SimpleJdbc {
+    private final Connection connection;
+
+    private SingleConnectionSimpleJdbc(
+        Connection connection,
+        ParameterSetters parameterSetters,
+        ColumnExtractors columnExtractors) {
+      super(parameterSetters, columnExtractors);
+      this.connection = connection;
+    }
+
+    @Override
+    public <T> T select(
+        String sql, Map<String, Object> bindings, QueryResultExtractor<T> extractor) {
+      return select(connection, sql, bindings, extractor);
+    }
+
+    @Override
+    public int update(String sql, Map<String, Object> bindings) {
+      return update(connection, sql, bindings);
+    }
+
+    @Override
+    public int[] batchedUpdate(String sql, List<Map<String, Object>> batchedBindings) {
+      return batchedUpdate(connection, sql, batchedBindings);
+    }
+  }
+
+  protected <T> T select(
       Connection conn,
       String sql,
       Map<String, Object> bindings,
@@ -47,15 +127,7 @@ public class SimpleJdbc {
     }
   }
 
-  int update(DataSource dataSource, String sql, Map<String, Object> bindings) {
-    try (Connection conn = dataSource.getConnection()) {
-      return update(conn, sql, bindings);
-    } catch (SQLException ex) {
-      throw new SimpleJdbcException(ex);
-    }
-  }
-
-  int update(Connection conn, String sql, Map<String, Object> bindings) {
+  protected int update(Connection conn, String sql, Map<String, Object> bindings) {
     ParameterizedQuery pq = ParameterizedQuery.from(sql, bindings);
     try (PreparedStatement stmt = conn.prepareStatement(pq.getSql())) {
       applyParameters(stmt, pq.getParameters());
@@ -65,15 +137,8 @@ public class SimpleJdbc {
     }
   }
 
-  int[] batchedUpdate(DataSource dataSource, String sql, List<Map<String, Object>> bindingsBatch) {
-    try (Connection conn = dataSource.getConnection()) {
-      return batchedUpdate(conn, sql, bindingsBatch);
-    } catch (SQLException ex) {
-      throw new SimpleJdbcException(ex);
-    }
-  }
-
-  int[] batchedUpdate(Connection conn, String sql, List<Map<String, Object>> bindingsBatch) {
+  protected int[] batchedUpdate(
+      Connection conn, String sql, List<Map<String, Object>> bindingsBatch) {
     if (bindingsBatch.isEmpty()) {
       throw new SimpleJdbcException("Empty batch");
     }
--- src/test/java/simplejdbc/SimpleJdbcTest.java
+++ src/test/java/simplejdbc/SimpleJdbcTest.java
@@ -18,58 +18,59 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-class SimpleJdbcTest {
-  
+abstract class SimpleJdbcTest {
+
   // TODO: QueryResultSet row-based?
   // TODO: Transactions
   // TODO: QueryBuilder, etc
 
-  private SimpleJdbc subject;
-  private DataSource dataSource;
-  private Connection connection;
+  protected Connection connection;
   private PreparedStatement preparedStatement;
   private ResultSet resultSet;
+  protected ParameterSetters parameterSetters;
+  protected ColumnExtractors columnExtractors;
+
+  abstract SimpleJdbc getSubject();
 
   @BeforeEach
-  void setUp() throws SQLException {
-    dataSource = Mockito.mock(DataSource.class);
+  void base_setup() throws SQLException {
+    parameterSetters = ParameterSetters.defaults();
+    columnExtractors = ColumnExtractors.defaults();
+
     connection = Mockito.mock(Connection.class);
     preparedStatement = Mockito.mock(PreparedStatement.class);
     resultSet = Mockito.mock(ResultSet.class);
 
-    when(dataSource.getConnection()).thenReturn(connection);
     when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
     when(preparedStatement.executeQuery()).thenReturn(resultSet);
-
-    subject = new SimpleJdbc(ParameterSetters.defaults(), ColumnExtractors.defaults());
   }
 
   @Test
   void select_SQLException_wrappedInCustomException() throws SQLException {
-    when(dataSource.getConnection()).thenThrow(new SQLException("test"));
+    when(connection.prepareStatement(anyString())).thenThrow(new SQLException("test"));
     SimpleJdbcException ex =
         assertThrows(
             SimpleJdbcException.class,
-            () -> subject.select(dataSource, "sql", ImmutableMap.of(), qr -> null));
+            () -> getSubject().select("sql", ImmutableMap.of(), qr -> null));
     assertThat(ex).hasCauseThat().isInstanceOf(SQLException.class);
   }
 
   @Test
   void update_SQLException_wrappedInCustomException() throws SQLException {
-    when(dataSource.getConnection()).thenThrow(new SQLException("test"));
+    when(connection.prepareStatement(anyString())).thenThrow(new SQLException("test"));
     SimpleJdbcException ex =
         assertThrows(
-            SimpleJdbcException.class, () -> subject.update(dataSource, "sql", ImmutableMap.of()));
+            SimpleJdbcException.class, () -> getSubject().update("sql", ImmutableMap.of()));
     assertThat(ex).hasCauseThat().isInstanceOf(SQLException.class);
   }
 
   @Test
   void batchedUpdate_SQLException_wrappedInCustomException() throws SQLException {
-    when(dataSource.getConnection()).thenThrow(new SQLException("test"));
+    when(connection.prepareStatement(anyString())).thenThrow(new SQLException("test"));
     SimpleJdbcException ex =
         assertThrows(
             SimpleJdbcException.class,
-            () -> subject.batchedUpdate(dataSource, "sql", ImmutableList.of(ImmutableMap.of())));
+            () -> getSubject().batchedUpdate("sql", ImmutableList.of(ImmutableMap.of())));
     assertThat(ex).hasCauseThat().isInstanceOf(SQLException.class);
   }
 
@@ -77,8 +78,7 @@ class SimpleJdbcTest {
   void batchedUpdate_withEmptyBatch_throws() {
     SimpleJdbcException ex =
         assertThrows(
-            SimpleJdbcException.class,
-            () -> subject.batchedUpdate(dataSource, "sql", ImmutableList.of()));
+            SimpleJdbcException.class, () -> getSubject().batchedUpdate("sql", ImmutableList.of()));
     assertThat(ex).hasMessageThat().contains("Empty batch");
   }
 
@@ -88,47 +88,41 @@ class SimpleJdbcTest {
         assertThrows(
             SimpleJdbcException.class,
             () ->
-                subject.batchedUpdate(
-                    dataSource,
-                    "select :foo",
-                    ImmutableList.of(
-                        ImmutableMap.of("foo", ImmutableList.of(1, 2)),
-                        ImmutableMap.of("foo", ImmutableList.of(1, 2, 3)))));
+                getSubject()
+                    .batchedUpdate(
+                        "select :foo",
+                        ImmutableList.of(
+                            ImmutableMap.of("foo", ImmutableList.of(1, 2)),
+                            ImmutableMap.of("foo", ImmutableList.of(1, 2, 3)))));
     assertThat(ex).hasMessageThat().contains("Invalid batch");
   }
 
   @Test
   void select_closesResources() throws SQLException {
-    subject.select(dataSource, "select 1", ImmutableMap.of(), qr -> null);
+    getSubject().select("select 1", ImmutableMap.of(), qr -> null);
 
     verify(resultSet).close();
     verify(preparedStatement).close();
-    verify(connection).close();
   }
 
   @Test
   void update_closesResources() throws SQLException {
-    subject.update(dataSource, "select 1", ImmutableMap.of());
+    getSubject().update("select 1", ImmutableMap.of());
 
     verify(preparedStatement).close();
-    verify(connection).close();
   }
 
   @Test
   void batchedUpdate_closesResources() throws SQLException {
-    subject.batchedUpdate(dataSource, "select 1", ImmutableList.of(ImmutableMap.of()));
+    getSubject().batchedUpdate("select 1", ImmutableList.of(ImmutableMap.of()));
 
     verify(preparedStatement).close();
-    verify(connection).close();
   }
 
   @Test
   void select_appliesParametersCorrectly() throws SQLException {
-    subject.select(
-        dataSource,
-        ":foo :bar :baz",
-        ImmutableMap.of("baz", 123, "bar", 456, "foo", 789),
-        qr -> null);
+    getSubject()
+        .select(":foo :bar :baz", ImmutableMap.of("baz", 123, "bar", 456, "foo", 789), qr -> null);
 
     verify(preparedStatement).setInt(1, 789);
     verify(preparedStatement).setInt(2, 456);
@@ -138,8 +132,7 @@ class SimpleJdbcTest {
 
   @Test
   void update_appliesParametersCorrectly() throws SQLException {
-    subject.update(
-        dataSource, ":foo :bar :baz", ImmutableMap.of("baz", 123, "bar", 456, "foo", 789));
+    getSubject().update(":foo :bar :baz", ImmutableMap.of("baz", 123, "bar", 456, "foo", 789));
 
     verify(preparedStatement).setInt(1, 789);
     verify(preparedStatement).setInt(2, 456);
@@ -149,12 +142,12 @@ class SimpleJdbcTest {
 
   @Test
   void batchedUpdate_appliesParametersCorrectly() throws SQLException {
-    subject.batchedUpdate(
-        dataSource,
-        ":foo :bar :baz",
-        ImmutableList.of(
-            ImmutableMap.of("baz", 123, "bar", 456, "foo", 789),
-            ImmutableMap.of("baz", 321, "bar", 654, "foo", 987)));
+    getSubject()
+        .batchedUpdate(
+            ":foo :bar :baz",
+            ImmutableList.of(
+                ImmutableMap.of("baz", 123, "bar", 456, "foo", 789),
+                ImmutableMap.of("baz", 321, "bar", 654, "foo", 987)));
 
     verify(preparedStatement).setInt(1, 789);
     verify(preparedStatement).setInt(2, 456);
@@ -168,18 +161,12 @@ class SimpleJdbcTest {
 
   @Test
   void select_queryResultExtractor_hasAccessToProvidedColumnExtractors() {
-    SimpleJdbc customSubject =
-        new SimpleJdbc(
-            ParameterSetters.defaults(),
-            ColumnExtractors.defaults()
-                .registerExtractor(Integer.class, (resultSet, columnLabel) -> 12345));
+    columnExtractors.registerExtractor(Integer.class, (resultSet, columnLabel) -> 12345);
 
     int returned =
-        customSubject.select(
-            dataSource,
-            "select 1",
-            ImmutableMap.of(),
-            queryResult -> queryResult.getInteger("whatever"));
+        getSubject()
+            .select(
+                "select 1", ImmutableMap.of(), queryResult -> queryResult.getInteger("whatever"));
 
     assertThat(returned).isEqualTo(12345);
   }
@@ -193,16 +180,81 @@ class SimpleJdbcTest {
         assertThrows(
             SimpleJdbcException.class,
             () ->
-                subject.select(
-                    dataSource,
-                    "select 1 as foo",
-                    ImmutableMap.of(),
-                    qr -> qr.toResultSet().getInt("foo")));
+                getSubject()
+                    .select(
+                        "select 1 as foo",
+                        ImmutableMap.of(),
+                        qr -> qr.toResultSet().getInt("foo")));
     assertThat(ex).hasCauseThat().hasMessageThat().isEqualTo(errorMessage);
   }
-  
-  @Test
-  void select_canReuseConnection() {
-    subject.select(connection, "some query", ImmutableMap.of(), qr -> null);
+
+  public static class DataSourceSimpleJdbcTest extends SimpleJdbcTest {
+    private SimpleJdbc subject;
+
+    @Override
+    SimpleJdbc getSubject() {
+      return subject;
+    }
+
+    @BeforeEach
+    void setup() throws SQLException {
+      DataSource dataSource = Mockito.mock(DataSource.class);
+      when(dataSource.getConnection()).thenReturn(connection);
+
+      subject = SimpleJdbc.using(dataSource, parameterSetters, columnExtractors);
+    }
+
+    @Test
+    @Override
+    void batchedUpdate_closesResources() throws SQLException {
+      super.batchedUpdate_closesResources();
+      verify(connection).close();
+    }
+
+    @Test
+    @Override
+    void update_closesResources() throws SQLException {
+      super.update_closesResources();
+      verify(connection).close();
+    }
+
+    @Test
+    @Override
+    void select_closesResources() throws SQLException {
+      super.select_closesResources();
+      verify(connection).close();
+    }
+  }
+
+  public static class SingleConnectionSimpleJdbcTest extends SimpleJdbcTest {
+    private SimpleJdbc subject;
+
+    @Override
+    SimpleJdbc getSubject() {
+      return subject;
+    }
+
+    @BeforeEach
+    void setup() {
+      subject = SimpleJdbc.using(connection, parameterSetters, columnExtractors);
+    }
+
+    @Test
+    void select_canReuseConnection() throws SQLException {
+      subject.select("some query", ImmutableMap.of(), qr -> null);
+      verify(connection, times(0)).close();
+    }
+
+    @Test
+    void update_canReuseConnection() throws SQLException {
+      subject.update("some query", ImmutableMap.of());
+      verify(connection, times(0)).close();
+    }
+
+    @Test
+    void batchedUpdate_canReuseConnection() throws SQLException {
+      subject.batchedUpdate("some query", ImmutableList.of(ImmutableMap.of()));
+      verify(connection, times(0)).close();
+    }
   }
 }
